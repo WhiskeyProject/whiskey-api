06393fcb79c50cc23d95c7fbfe02d3d05045a999
Aaron Scruggs <aarondscruggs@gmail.com>
2016-04-27 12:46:02 -0700

    Created models and serializers. Switched origin to new organization

--- whiskies/models.py
+++ whiskies/models.py
@@ -1,3 +1,82 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class Whiskey(models.Model):
+    title = models.CharField(max_length=255)
+    img_url = models.URLField(null=True, blank=True)
+    description = models.TextField()
+
+    # These will be tied to live data in the future.
+    price = models.IntegerField()
+    rating = models.IntegerField()
+
+    class Meta:
+        default_related_name = "whiskies"
+
+
+class Profile(models.Model):
+
+    # https://docs.djangoproject.com/es/1.9/topics/db/examples/many_to_many/
+
+    user = models.OneToOneField(User, null=True)
+
+    liked_whiskies = models.ManyToManyField(Whiskey)
+    disliked_whiskies = models.ManyToManyField(Whiskey)
+    #searches = models.ManyToManyField(TagSearch)
+
+
+class Review(models.Model):
+
+    user = models.ForeignKey(User)
+    whiskey = models.ForeignKey(Whiskey)
+    text = models.TextField()
+    rating = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        default_related_name = "reviews"
+
+
+class TagSearch(models.Model):
+    user = models.ForeignKey(User)
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        default_related_name = "tag_searches"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+    whiskies = models.ManyToManyField(Whiskey, through="TagTracker")
+
+    class Meta:
+        default_related_name = "tags"
+
+    # overwrite save.
+        # if not self.pk get or create tagtracker
+        # call add_count
+        # return rest of the save
+    # overwrite delete
+        # decrement tag tracker
+
+
+class TagTracker(models.Model):
+    count = 0
+
+    whiskey = models.ForeignKey(Whiskey)
+    tag = models.ForeignKey(Tag)
+
+    def add_count(self):
+        self.count += 1
---
+++ whiskies/serializers.py
@@ -0,0 +1,81 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from whiskies.models import Whiskey, Profile, Review, TagSearch, Tag,\
+    TagTracker
+
+
+class WhiskeySerializer(serializers.ModelSerializer):
+    reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    # might need to go through TagTracker
+    tags = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Whiskey
+        fields = "__all__"
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    # check that liked and disliked whiskies are displayed
+
+
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    tag_searches = serializers.PrimaryKeyRelatedField(many=True,
+                                                      read_only=True)
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    # Set user and whiskey in the view. Not sure on whiskey though.
+
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    whiskey = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class TagSerializer(serializers.ModelSerializer):
+    # I don't think tag cares about tag_searches
+
+    # set whiskey in the view when a tag is created.
+
+    whiskies = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+
+class TagSearchSerializer(serializers.ModelSerializer):
+
+    #don't forget to set profile in view
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    tags = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = TagSearch
+        fields = "__all__"
+
+
+class TagTrackerSerializer(serializers.ModelSerializer):
+
+    # Not sure if this is needed for anything.
+
+    class Meta:
+        model = TagTracker
+        fields = "__all__"
