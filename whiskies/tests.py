77d941fcf0882a6ee9c8a76d07ba8fe4abfffb24
Aaron Scruggs <aarondscruggs@gmail.com>
2016-04-28 14:34:19 -0700

    Added User, Whiskey, Review tests

--- whiskies/tests.py
+++ whiskies/tests.py
@@ -1,3 +1,93 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from rest_framework.authtoken.models import Token
+from whiskies.models import Whiskey, Review, Profile
+
+
+class UserTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="Tester",
+                                             password="pass_word")
+
+        self.url = reverse("list_users")
+
+    def test_create_user(self):
+        data = {"username": "Tester2", "password": "pass_word",
+                "email": "test@gmail"}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)
+
+        self.assertEqual(Token.objects.count(), User.objects.count())
+
+    def test_retrieve_user(self):
+        response = self.client.get(self.url,{"pk": self.user.id},
+                                   format="json")
+        self.assertEqual(response.data[0]['username'], self.user.username)
+
+        profile = response.data[0].get('profile', None)
+        self.assertNotEqual(profile, None)
+
+
+class WhiskeyTest(APITestCase):
+
+    def setUp(self):
+        self.whiskies = [
+            Whiskey.objects.create(title="test", price=x, rating=x)
+            for x in range(1, 4)
+            ]
+
+    def test_retrieve_whiskey_list(self):
+        url = reverse("list_whiskey")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), Whiskey.objects.count())
+
+    def test_retrieve_whiskey(self):
+        url = reverse("list_whiskey")
+        whiskey = self.whiskies[0]
+        response = self.client.get(url, {"pk": whiskey.id}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(whiskey.price, response.data[0].get('price', None))
+
+
+class ReviewTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="Tester",
+                                             password="pass_word")
+        self.whiskey = Whiskey.objects.create(title="test", price=5, rating=5)
+
+        self.review = Review.objects.create(user=self.user,
+                                            whiskey=self.whiskey,
+                                            text="this is a test review")
+
+    def test_list_reviews(self):
+        url = reverse("list_review")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), Review.objects.count())
+
+    def test_retrieve_review(self):
+        url = reverse("list_review")
+        response = self.client.get(url, {"pk": self.review.id}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.review.text, response.data[0].get('text', None))
+
+    def test_create_review(self):
+        url = reverse("list_review")
+
+        token = Token.objects.get(user_id=self.user.id)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
+        data = {"whiskey": self.whiskey.id,
+                "text": "testing"}
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Review.objects.count(), 2)
