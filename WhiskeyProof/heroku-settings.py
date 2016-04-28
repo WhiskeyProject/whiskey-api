808af1afaf4a9440ac716b0beb3dbede849a447c
Aaron Scruggs <aarondscruggs@gmail.com>
2016-04-28 11:10:47 -0700

    Setup for Heroku deployment

---
+++ WhiskeyProof/heroku-settings.py
@@ -0,0 +1,25 @@
+from .settings import *
+import dj_database_url
+
+
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+# make sure to include any directories that might be outside of the app/static directories.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../global/'),
+)
+
+
--- WhiskeyProof/settings.py
+++ WhiskeyProof/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4$hq0n_7bm1vn822n)#3pr9j(a&4lo(wac37jk(@gh9#0p8mio'
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
