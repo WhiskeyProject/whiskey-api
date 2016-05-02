7e6da4a9ec1984a4677d860ca5cc27d97461fa42
Aaron Scruggs <aarondscruggs@gmail.com>
2016-05-02 10:17:38 -0700

    Set cloudinary secret on travis

--- WhiskeyProof/settings.py
+++ WhiskeyProof/settings.py
@@ -82,17 +82,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'WhiskeyProof.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
