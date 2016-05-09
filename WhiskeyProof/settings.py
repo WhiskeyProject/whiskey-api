264f443fc4303527d0d7f3161bc8f47d1fae2b81
Aaron Scruggs <aarondscruggs@gmail.com>
2016-05-09 11:51:49 -0700

    Adding pagination

--- WhiskeyProof/settings.py
+++ WhiskeyProof/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 5,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication')
