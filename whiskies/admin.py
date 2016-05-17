2d9b783707620714d78e68507ea40bdfdf8a27b0
Aaron Scruggs <aarondscruggs@gmail.com>
2016-05-17 14:05:27 -0700

    Added list_display_links in admin

--- whiskies/admin.py
+++ whiskies/admin.py
@@ -11,6 +11,9 @@ class WhiskeyAdmin(admin.ModelAdmin):
                     "img_url")
 
     search_fields = ["title", "description", "region"]
+    
+    list_display_links = ("id", "title", "img_url", "price", "rating",
+                          "region", "review_count")
 
 
 @admin.register(Tag)
